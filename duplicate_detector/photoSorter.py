1e6b79b36f453debd7e12da0cfb34cb8a9fa17b2
Nathan Malnoury <n.malnoury@gmail.com>
2020-07-04 15:00:16 +0200

    Some more pep8

--- duplicate_detector/photoSorter.py
+++ duplicate_detector/photoSorter.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
+
 from tqdm import tqdm
+
 from .photofile import PhotoFile
 
 
@@ -12,21 +14,21 @@ class PhotoSorter:
 
     def __init__(self, dir_content, dest):
         self.content = dir_content
-        self.dest = dest
+        self.destination = dest
         self.no_metadata = set()
 
     def tidy(self):
         for filename in tqdm(self.content):
             extension = self.get_extension(filename)
             if extension in self.extensions_allowed:
-                file = PhotoFile(filename, self.content[filename], self.dest)
+                file = PhotoFile(filename, self.content[filename], self.destination)
                 self.no_metadata = file.tidy(no_metadata_set=self.no_metadata)
 
-        with open(Path(self.dest) / "a_trier.json", "w+") as f:
+        with open(Path(self.destination) / "a_trier.json", "w+") as f:
             json.dump(list(self.no_metadata), f, indent=2)
 
         print(f"{len(self.no_metadata)} files to sort by hand are in a file, "
-              f"'a_trier.json' in {self.dest}")
+              f"'a_trier.json' in {self.destination}")
 
     @staticmethod
     def get_extension(file: str):
